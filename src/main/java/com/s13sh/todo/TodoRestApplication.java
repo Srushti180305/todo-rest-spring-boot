5954ad1218c99e037664e8d5573411c5ff68f14e
Srushti180305 <lingshettersrushti@gmail.com>
2026-05-13 12:56:46 +0530

    Add frontend HTML and auto open browser on startup

--- src/main/java/com/s13sh/todo/TodoRestApplication.java
+++ src/main/java/com/s13sh/todo/TodoRestApplication.java
@@ -2,12 +2,29 @@ package com.s13sh.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.awt.Desktop;
+import java.net.URI;
 
 @SpringBootApplication
 public class TodoRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoRestApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TodoRestApplication.class, args);
 
+        // Force open in external browser
+        String url = "http://localhost:8055";
+        try {
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start", url});
+            } else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                Runtime.getRuntime().exec(new String[]{"open", url});
+            } else {
+                Runtime.getRuntime().exec(new String[]{"xdg-open", url});
+            }
+        } catch (Exception e) {
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(new URI(url));
+            }
+        }
+    }
 }
